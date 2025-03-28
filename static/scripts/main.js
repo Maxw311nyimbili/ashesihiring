52c4a5b2617b663e10874c6d2eb23e0912d1c8d5
Maxw311 <nyimbilimaxwell9@gmail.com>
2025-03-28 01:10:15 +0000

    initial commit

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -184,8 +184,8 @@ document.getElementById("post-comment-btn").addEventListener("click", function (
             <span id="commentText-${commentId}" style="color: inherit;">${commentText}</span>
         </p>
         <small class="text-muted">
-            <button class="btn btn-sm btn-primary" onclick="editComment('${commentId}')">Edit</button>
-            <button class="btn btn-sm btn-danger" onclick="deleteComment('${commentId}')">Delete</button>
+            <button class="btn btn-sm" style="color:#236465 !important" onclick="editComment('${commentId}')">Edit</button>
+            <button class="btn btn-sm" style="color:#AD4245 !important" onclick="deleteComment('${commentId}')">Delete</button>
         </small>
     `;
 
